d6ba05f9cb063999b5b1c4211286347d3b8db795
JP <jonathankbyers@gmail.com>
2024-02-21 11:22:38 -0500

    init

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const PORT = process.env.PORT || 3000
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_STRING, {
